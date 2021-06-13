f44643f08e8537637a3dd34b4b416e818a86fd02
DontiRohit <65717106+DontiRohit@users.noreply.github.com>
2021-06-13 09:51:30 +0530

    my first commit

---
+++ app/src/main/java/com/example/kotlindemo/Main.kt
@@ -0,0 +1,10 @@
+package com.example.kotlindemo
+
+fun main()
+{
+    var myString:String="Android Kotlin"
+    var myFloat:Float=13.37F
+    var myDouble:Double=3.14159265358979
+    var myInt:Int=25
+    print("String= $myString, Float= $myFloat, Double= $myDouble, Int= $myInt")
+}
